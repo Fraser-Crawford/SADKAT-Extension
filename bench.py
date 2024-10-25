Corrected mfs calculation in radial, method now works when using around 10 layers

bench.py
@@ -33,27 +33,42 @@ def plot_solution(solution):
     plt.show()
 
 if __name__ == '__main__':
-    layers = 100
-    temp = 293
-    radial = RadialDroplet.from_mfs(viscous_aqueous_NaCl, Atmosphere(temp), np.array([0, 0, 0]), 50e-6, 0.5, temp, layers)
+    layers = 10
+    temp = 313
+    print("Start")
+    radial = RadialDroplet.from_mfs(viscous_aqueous_NaCl, Atmosphere(temp), np.array([0, 0, 0]), 50e-6, 0.1, temp, layers)
     trajectory = radial.integrate(2)
+    print("integrated")
     df = radial.complete_trajectory(trajectory)
     plt.plot(df.time,df.radius)
     positions = []
     concentrations = []
+
     for i in range(layers-1):
         positions += [[df["layer_boundaries"].values[j][i] for j in range(len(df["layer_boundaries"].values))]]
         plt.plot(df.time,positions[-1])
     positions += [[df["radius"].values[i] for i in range(len(df["radius"].values))]]
+    print("positions set")
     for i in range(layers):
         concentrations += [[df["layer_concentration"].values[j][i] for j in range(len(df["layer_concentration"].values))]]
-    uniform = UniformDroplet.from_mfs(aqueous_NaCl, Atmosphere(temp), np.array([0, 0, 0]), 50e-6, 0.5, temp, )
+
+
+    uniform = UniformDroplet.from_mfs(aqueous_NaCl, Atmosphere(temp), np.array([0, 0, 0]), 50e-6, 0.1, temp, )
     trajectory = uniform.integrate(2)
     df2 = uniform.complete_trajectory(trajectory)
     plt.scatter(df2.time, df2.radius)
-
     plt.show()
+
     for r,conc in zip(positions,concentrations):
         plt.plot(df.time,conc)
     plt.scatter(df2.time,df2.concentration)
+    plt.show()
+
+    plt.plot(df.time,df.surface_diffusion)
+    plt.show()
+    mfss = []
+    for i in range(layers):
+        mfss += [[df["layer_mass_fraction_solute"].values[j][i] for j in
+                  range(len(df["layer_mass_fraction_solute"].values))]]
+        plt.plot(df.time,mfss[i])
     plt.show()

radial.py
@@ -8,7 +8,7 @@ from droplet import Droplet
 from uniform import UniformDroplet
 from viscous_solution import ViscousSolution
 
-layer_inertia = 1.0
+layer_inertia = 0.1
 stiffness = 10.0
 damping = 2*np.sqrt(stiffness*layer_inertia)
 
@@ -19,6 +19,10 @@ class RadialDroplet(Droplet):
             layer_mass_solute=self.layer_mass_solute[:],
             layer_concentration=self.layer_concentration,
             layer_boundaries=self.cell_boundaries,
+            surface_concentration=self.layer_concentration[-1],
+            average_diffusion = self.solution.diffusion(self.mass_fraction_solute,self.temperature),
+            surface_diffusion = self.solution.diffusion(self.layer_mass_fraction_solute[-1],self.temperature),
+            layer_mass_fraction_solute = self.layer_mass_fraction_solute[:]
         )
 
     solution: ViscousSolution
@@ -114,9 +118,15 @@ class RadialDroplet(Droplet):
                 result[i+1] -= value
 
         return result
+
+    @property
+    def layer_density(self):
+        return self.solution.concentration_to_solute_mass_fraction(self.layer_concentration)
+
     @property
     def layer_mass_fraction_solute(self):
-        return self.layer_mass_solute/(self.layer_mass_solute+self.mass_solvent())
+        concentration = self.layer_mass_solute/self.layer_volume
+        return self.solution.concentration_to_solute_mass_fraction(concentration)
 
     def get_gradients(self,normalised_boundaries):
         concentrations = self.layer_concentration

solution.py
@@ -20,6 +20,8 @@ class Solution:
     # default) to assume Raoult's law for ideal mixtures
     activity: Callable[[float | npt.NDArray[np.float_]], float | npt.NDArray[np.float_]]
     solid_refractive_index: float
+    concentration_coefficients = None
+    density_derivative_coefficients = None
 
     def mole_fraction_solute(self, mass_fraction_solute):
         """Mole fraction from mass fraction."""
@@ -32,8 +34,19 @@ class Solution:
         return 1 - self.mole_fraction_solute(mass_fraction_solute)
 
     def concentration_to_solute_mass_fraction(self, concentration):
-        mfss = np.linspace(0, 1.0, 101)
-        return np.interp(concentration, [self.density(mfs)*mfs for mfs in mfss], mfss)
+        if self.concentration_coefficients is None:
+            mfs = np.linspace(0,1.0,100)
+            concentrations = mfs*self.density(mfs)
+            self.concentration_coefficients = np.polyfit(concentrations, mfs, 4)
+        return np.poly1d(self.concentration_coefficients)(concentration)
 
     def concentration(self, mfs):
         return mfs * self.density(mfs)
+
+    def density_derivative(self, mfs):
+        if self.density_derivative_coefficients is None:
+            mfs = np.linspace(0,1.0,100)
+            densities = self.density(mfs)
+            density_gradients = np.gradient(densities,mfs)
+            self.density_derivative_coefficients = np.polyfit(mfs, density_gradients, 4)
+        return np.poly1d(self.density_derivative_coefficients)(mfs)