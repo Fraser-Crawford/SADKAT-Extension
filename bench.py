Corrected the sign on the damping equation, layers relax a lot quicker now.

bench.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     temp = 313
     print("Start")
     radial = RadialDroplet.from_mfs(viscous_aqueous_NaCl, Atmosphere(temp), np.array([0, 0, 0]), 50e-6, 0.1, temp, layers)
-    trajectory = radial.integrate(2)
+    trajectory = radial.integrate(1)
     print("integrated")
     df = radial.complete_trajectory(trajectory)
     plt.plot(df.time,df.radius)
@@ -54,7 +54,7 @@ if __name__ == '__main__':
 
 
     uniform = UniformDroplet.from_mfs(aqueous_NaCl, Atmosphere(temp), np.array([0, 0, 0]), 50e-6, 0.1, temp, )
-    trajectory = uniform.integrate(2)
+    trajectory = uniform.integrate(1)
     df2 = uniform.complete_trajectory(trajectory)
     plt.scatter(df2.time, df2.radius)
     plt.show()

radial.py
@@ -9,8 +9,8 @@ from uniform import UniformDroplet
 from viscous_solution import ViscousSolution
 
 
-layer_inertia = 1.5
-stiffness = 7.5
+layer_inertia = 1
+stiffness = 100
 damping = 2.0*np.sqrt(stiffness*layer_inertia)
 
 @dataclass
@@ -89,7 +89,7 @@ class RadialDroplet(Droplet):
         return self.cell_boundaries - self.radius*np.array([i/self.layers for i in range(1,self.layers)])
 
     def boundary_acceleration(self):
-        return (self.cell_velocities*damping-stiffness*self.deviation/self.radius)/layer_inertia
+        return (-self.cell_velocities*damping-stiffness*self.deviation/self.radius)/layer_inertia
 
     def boundary_correction(self):
         return self.cell_velocities